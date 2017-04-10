8d83e492894bdf559a0b1d11cb3e0384836d69e1
dimda <daviskemlers@gmail.com>
2017-04-10 21:46:47 +0900

    Display moisture level on lcd

--- src/plant-watcher.ino
+++ src/plant-watcher.ino
@@ -1,10 +1,33 @@
+#include "Adafruit_SSD1306.h"
+
+//Display settings
+#define OLED_DC     D3
+#define OLED_CS     D4
+#define OLED_RESET  D5
+Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
+
 int moistureLevel = 0;
-void setup() {
-  pinMode(A4, INPUT);
+void setup()   {
+  //Initialize moisture sensor
+  pinMode(A0, INPUT);
   Particle.variable("moistureLvl", moistureLevel);
+
+  //Initialize display
+  display.begin(SSD1306_SWITCHCAPVCC);
 }
 
 void loop() {
-  moistureLevel = analogRead(A4);
-  delay(200);
+  moistureLevel = analogRead(A0);
+  displayMoisture(moistureLevel);
+  delay(2000);
+}
+
+void displayMoisture(int moistureLevel) {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Moisture:");
+  display.println(moistureLevel);
+  display.display();
 }
