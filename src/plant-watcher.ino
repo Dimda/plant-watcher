2ad59a695895b133f6c3aa6400b93b7acd1a4e75
Davis Kemlers <daviskemlers@gmail.com>
2017-04-25 16:11:13 +0900

    Add support for 2 moisture sensors

--- src/plant-watcher.ino
+++ src/plant-watcher.ino
@@ -6,28 +6,39 @@
 #define OLED_RESET  D5
 Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
 
-int moistureLevel = 0;
+int moistureLevel1 = 0;
+int moistureLevel2 = 0;
+
+int moistureSensor1 = A0;
+int moistureSensor2 = A1;
+
 void setup()   {
   //Initialize moisture sensor
-  pinMode(A0, INPUT);
-  Particle.variable("moistureLvl", moistureLevel);
+  pinMode(moistureSensor1, INPUT);
+  pinMode(moistureSensor2, INPUT);
+  Particle.variable("moistureLvl1", moistureLevel1);
+  Particle.variable("moistureLvl2", moistureLevel2);
 
   //Initialize display
   display.begin(SSD1306_SWITCHCAPVCC);
 }
 
 void loop() {
-  moistureLevel = analogRead(A0);
-  displayMoisture(moistureLevel);
+  moistureLevel1 = analogRead(moistureSensor1);
+  moistureLevel2 = analogRead(moistureSensor2);
+  displayMoisture(moistureLevel1, moistureLevel2);
+
   delay(2000);
 }
 
-void displayMoisture(int moistureLevel) {
+void displayMoisture(int moistureLevel1, int moistureLevel2) {
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.println("Moisture:");
-  display.println(moistureLevel);
+  display.println("M1:");
+  display.println(moistureLevel1);
+  display.println("M2:");
+  display.println(moistureLevel2);
   display.display();
 }
